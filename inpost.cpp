8835fb7c6439640112116ea527832f3509c03fe4
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-14 10:04:33 -0300

    Update inpost.cpp

--- inpost.cpp
+++ inpost.cpp
@@ -1,29 +1,20 @@
-/*
-	inpost.cpp
-	Date Written: 3/18
-	Created by Kyin Edwards
-*/
-
 #include <string>
 #include <cctype>
 #include "mystack.h"
 #include "inpost.h"
 
-/*
-	Convert member function takes the infix expression and passes it as a C++ string of an equivalent postfix expression stored in a string object.
-
-	@param infix portion of the expression stored in the string object ((input) expression tokens). 
-		
-	@return postfix string is returned after conversion from infix. 
-*/
-
+/** Convert infix expression.
+ *
+ * @param infix: Portion of the expression stored in the string object. 
+ * @return: Conversion from infix. 
+ */
 std::string convert(const std::string& infix) {
-	std::string postfix; // Output expression will be constructed
-	mystack s; // Used to hold operators during conversion. 
-	
 	size_t index = 0;
+	std::string postfix; 
+	mystack s;  
+
 	
-	// While not at the end of the infix string
+	// Get to the end of the infix string.
 	while (index < infix.length()) {
 		// Append current character to postfix string.
 		// Advance to the next character of the infix string. 
@@ -39,7 +30,6 @@ std::string convert(const std::string& infix) {
 		else if (isdigit(infix[index])) {
 			while (isdigit(infix[index])) {
 				postfix += infix[index];
-				// postfix += ' ';
 				index++;
 			}
 			
@@ -60,24 +50,22 @@ std::string convert(const std::string& infix) {
 		else if (infix[index] == ')') {
 			while (infix[index] != '(' && !s.empty()) {
 				// Top character from the stack. 
-				
 				// Append the character to the postfix string. 
 				postfix += infix[index];
 				postfix += ' ';
-				
 				s.pop();
 			}
 			
+			// Top character is the left parenthesis. 			
 			if (!s.empty()) {
-				// Top character is the left parenthesis. 
-				s.pop(); // postfix.pop();
+				s.pop();
 			}
 			
 			index++;
 		}
-		
+	
+		// This is an operator		
 		else {
-			// This is an operator
 			while (!s.empty() && precedence(infix[index]) <= precedence(s.top())) {
 				postfix += s.top();
 				s.pop();
@@ -88,15 +76,13 @@ std::string convert(const std::string& infix) {
 		}
 	}
 	
-	// Stack is not empty. 
+	// Get top character of the stack and append to postfix string. 	 
 	while (!s.empty()) {
-		// Get top character of the stack and append to postfix string. 
 		postfix += s.top();
 		s.pop();
 	}
 	
 	s.pop();
-	
 	return postfix;
 }
 
