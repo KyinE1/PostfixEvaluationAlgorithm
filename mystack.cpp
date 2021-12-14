4a148578fe1c527ee7a555d5b98efee6f8ba923e
KyinE1 <83590743+KyinE1@users.noreply.github.com>
2021-12-14 10:03:15 -0300

    Update mystack.cpp

--- mystack.cpp
+++ mystack.cpp
@@ -1,150 +1,101 @@
-/*
-	mystack.cpp
-	Date Written: 3/18
-	Created by Kyin Edwards
-*/
-
 #include "mystack.h"
 
-/*
-	Default Constructor.  
-*/ 
+/** Constructor. */ 
 mystack::mystack() {
 	stack_size = 0;
 	stack_top_pointer = nullptr;
 }
 
-/*
-	Copy constructor for mystack class initializes a new mystack object to the same string as the existing mystack object x.
-	
-	@param x object of the mystack class.
-*/
-mystack::mystack(const mystack& x) {
-	// if (stack_size == 0)
-		// stack_top_pointer = nullptr;
-		
-	// Set the new stack object's list to empty.  
+/** Copy constructor. Sets attributes of the stack.
+ *
+ * @param x: mystack class object.
+ */
+mystack::mystack(const mystack& x) {  
 	stack_top_pointer = nullptr;
-	
-	// Copy the other stack's size. 
-	stack_size = x.stack_size;
-	
-	// Copy the other stack's linked list. 
+	stack_size = x.stack_size; 
 	clone(x);
 }
 
-/*
-	The destructor makes a call to the clear() method. 
-*/
+/** Destructor. */
 mystack::~mystack() {
 	clear();
 }
 
-/*
-	Assigns one mystack object (x) to the object that called the member function (pointed to by this).
-	
-	@param x object of the mystack class. 
-	
-	@return address of the object x.  
+/** Assigns one mystack object (x) to the object that called the member function.
+ *
+ * @param x: mystack class object. 
+ * @return: Address of the object.  
 */
 mystack& mystack::operator=(const mystack& x) {
-	// Check for self-assignment.
     if (this != &x) {
-        // Left stack object is made empty. 
         clear();
-
-        // Copy the other stack's size. 
-        stack_size = x.stack_size;
-		
-		// Copy the other stack's linked list. 
+        stack_size = x.stack_size; 
 		clone(x);
     }
 	
-	// Return this object (return *this).
+	// Return this object.
 	return *this;
 }
 
-/*
-	Get the size of the stack.  
-	
-	@return the stack_size. 
-*/
+/** Accessor method. */
 size_t mystack::size() const {
 	return stack_size;
 }
 
-/*
-	Evaluates if the stack_size is empty. 
-	
-	@return true if the stack_size is 0, else false. 
-*/
+/** Evaluates if the stack_size is empty.
+ * 
+ * @return: True if the size is 0.  
+ */
 bool mystack::empty() const {
 	return (stack_size == 0);
 }
 
-/*
-	Reset the stack_size to default value and delete all nodes in the stack's link list.
-*/
+/** Reset the size to default value and delete all nodes in the stack's linked list. */
 void mystack::clear() {
-	// As long as the stack is not empty, pop() method is called. 
 	while (!empty())
 		pop();
 }		
 
-/*
-	Return value in the top node of the stack (first node in the linked list pointed to by the stack_top_pointer).
-		
-	@return the stack_top_pointer->value (pointer to value).
-*/
+/** Get the first node's value from the stack.
+ *
+ * @return: Value of the top of the stack.
+ */
 const int& mystack::top() const {
 	return stack_top_pointer->value;
 }
 
-/*
-	Push integer value onto the top of the stack. 
-*/
+/** Push integer value onto the top of the stack. */
 void mystack::push(int value) {
-	// Allocation of a new stack node needs to call the struct.
 	node* new_node = new node(value, stack_top_pointer);
 
 	new_node->value = value;
 	new_node->next = stack_top_pointer;
-	
 	stack_top_pointer = new_node;
-	
 	stack_size = stack_size + 1; 
 }
 
-/*
-	Pop the top item off of the stack and delete the node that contained it (the top item is removed from the stack).
-*/
+/** Pop the top item off of the stack and delete the node that contained it. */
 void mystack::pop() {
-	// Temp node pointer.
 	node* delete_node = stack_top_pointer;
 	stack_top_pointer = stack_top_pointer->next;
-	
-	// Delete the node. 
-	delete delete_node; // Or delete[] delete_node.
-	
-	stack_size = stack_size - 1; // Or stack_size--;
+	 
+	delete delete_node; 
+	stack_size = stack_size - 1;
 }
 
-/*
-	Copies linked list from the stack x to this object.
-	
-	@param x object of the mystack class. 
-*/
+/** Copies linked list from the stack to this object.
+ *	
+ * @param x: Object of the mystack class. 
+ */
 void mystack::clone(const mystack& x) {
 	node* last = nullptr;
 	node* ptr = x.stack_top_pointer; 
 	
 	while (ptr != nullptr) {
-		// Allocation of a new stack node needs to call the stuct. 
 		node* new_node = new node(ptr->value);
 		
 		if (last == nullptr)
-			stack_top_pointer = new_node;
-			
+			stack_top_pointer = new_node;			
 		else
 			last->next = new_node; 
 			
